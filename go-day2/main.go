36d39a256ebed01383a2f2d0073f93642b661ab3
nazimjrf <nazim@jrfintel.com>
2026-01-03 08:41:08 +0600

    day-2 completed

---
+++ go-day2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variables
+	num1 := 20
+	num2 := 8.2
+
+	// Addition
+	sum := float64(num1) + num2
+
+	// Subtraction
+	diff := float64(num1) - num2
+
+	fmt.Println("Number 1:", num1)
+	fmt.Println("Number 2:", num2)
+	fmt.Println("Addition:", sum)
+	fmt.Println("Subtraction:", diff)
+}
+
