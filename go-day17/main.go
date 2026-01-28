888acee6e089cdeb4da55a928d68d4e965b82ebd
nazimjrf <nazim@jrfintel.com>
2026-01-28 09:42:14 +0600

    updated day 17  & 18

---
+++ go-day17/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("cannot divide by zero")
+	}
+	return a / b, nil
+}
+
+func main() {
+	result, err := divide(10, 0)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Result:", result)
+}
+
---
+++ go-day18/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"day18/mathutils"
+)
+
+func main() {
+	fmt.Println("Add:", mathutils.Add(10, 5))
+	fmt.Println("Sub:", mathutils.Sub(10, 5))
+}
